93e7631eabdd389c5dc65922216fcf2f8a8e8023
rudy <argmend@gmail.com>
2020-11-27 21:38:02 -0600

    initial push

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import Wrapper from './components/Wrapper';
+import Header from './components/Header';
+import UsersTable from './components/UsersTable';
+import './App.css';
+
+function App() {
+  return (
+    <Wrapper>
+      <Header></Header>
+      <UsersTable></UsersTable>
+    </Wrapper>
+  );
+}
+
+export default App;
